987ee54dbb76918a2abb6b191b78bf17be9387ff
Paul <41129807+Paulanerus@users.noreply.github.com>
2024-03-18 21:42:04 +0100

    Add Command class and update ArgParser class

--- src/arg_parser.hpp
+++ src/arg_parser.hpp
@@ -1,18 +1,51 @@
 #pragma once
 
+#include <type_traits>
 #include <algorithm>
 #include <iostream>
 #include <charconv>
+#include <cstdint>
 #include <string>
 #include <vector>
+#include <memory>
+
+class Command : std::enable_shared_from_this<Command>
+{
+public:
+    Command() noexcept = default;
+
+    bool operator[](std::string_view strIn) const noexcept
+    {
+        return false;
+    }
+
+private:
+};
 
 class ArgParser
 {
 public:
-    ArgParser(int argc, char *argv[]) : m_Args(argv + 1, argv + argc)
+    ArgParser() noexcept = default;
+
+    template <typename... Args, typename = std::enable_if_t<std::conjunction_v<std::is_same<Args, std::string>...>>>
+    std::unique_ptr<Command> command(Args &&...) noexcept
     {
+        return std::make_unique<Command>(std::forward<Args>(args)...);
+    }
+
+    std::vector<std::string> parse(const char *argv[], int argc, size_t start = 1)
+    {
+        std::vector<std::string> args{argv + start, argv + argc};
+
+        std::for_each(args.begin(), args.end(), [](std::string &arg)
+                      { std::transform(arg.begin(), arg.end(), arg.begin(), [](uint8_t c)
+                                       { return std::tolower(c); }); });
+
+        for (auto &arg : args)
+            std::cout << arg << std::endl;
+
+        return args;
     }
 
 private:
-    std::vector<std::string> m_Args;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,37 @@
 
 int main(int argc, char *argv[])
 {
-    ArgParser parser{argc, argv};
-    
+    using namespace std::string_literals;
+
+    const char *arguments[] = {"add", "-l", "-n", "test", "-p"};
+
+    ArgParser parser;
+
+    parser.command("add"s, "a"s);
+
+    /*
+    parser.command("Help", "me");
+
+    parser.command("add", "a")
+        .help("Adds a library to the project")
+
+        .argument("link", "l")
+            .flag()
+            .help("Marks the added library as a link library")
+            .end()
+
+        .argument("name", "n")
+            .required()
+            .help("The name of the library to add")
+            .end();
+
+        .argument("path", "p");
+            .default(".")
+            .help("The path to the library to add")
+            .end();
+    */
+
+    parser.parse(arguments, 5, 0);
 
     return EXIT_SUCCESS;
 }
